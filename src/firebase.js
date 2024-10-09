5be0cbb85a2c9ae85d288e33862ed4b489165580
Sultan Muhammad Dhavi <dhavisrg@Sultans-MacBook-Air.local>
2024-10-09 20:32:30 +0700

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQuZ8dwgAotJHZLkyrM49vV9MgJSrzc0k",
-  authDomain: "netflix-clone-d8598.firebaseapp.com",
-  projectId: "netflix-clone-d8598",
-  storageBucket: "netflix-clone-d8598.appspot.com",
-  messagingSenderId: "405712255468",
-  appId: "1:405712255468:web:16cf76fbf310c780c6e8c9",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
